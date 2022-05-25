0d33df2e615e2b2b9ddf1d0522d2b779cf1bd0a7
FelipeRotermel <feliperotermell@gmail.com>
2022-05-25 14:44:34 -0300

    salvar

--- src/components/CorpoPrincipal.vue
+++ src/components/CorpoPrincipal.vue
@@ -1,8 +1,20 @@
 <script></script>
 <template>
   <main id="corpo">
+    <div class="logo">
+      <img class="image-logo" src="@/assets/imgs/Logo.png" alt="logo" />
+    </div>
     <div class="input-group">
-      <input type="text" placeholder="Search" class="Input text" />
+      <input
+        type="text"
+        placeholder="Search"
+        class="Input text input-pesquisa"
+        value="Homem Aranha 3"
+      />
+    </div>
+    <div class="resultado">
+      <img src="@/assets/imgs/Homem-aranha3.jpg" />
+      <p class="resultados">Netflix</p>
     </div>
   </main>
 </template>
@@ -12,5 +24,13 @@
   justify-content: center;
   align-items: center;
   color: black;
+}
+
+.image-logo {
+  width: 250px;
+  height: 50px;
+}
+.logo {
+  margin-top: 10%;
 }
 </style>
